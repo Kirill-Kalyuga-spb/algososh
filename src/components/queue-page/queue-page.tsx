180f9901fcab354b5076d41667ca83f958a360a2
Kirill-Kalyuga-spb <Chelik228fruit@yandex.ru>
2023-05-25 22:18:39 +0300

    stack-page created

--- src/components/queue-page/queue-page.tsx
+++ src/components/queue-page/queue-page.tsx
@@ -1,10 +1,106 @@
-import React from "react";
+import React, { ChangeEvent, useState } from "react";
 import { SolutionLayout } from "../ui/solution-layout/solution-layout";
+import { Button } from "../ui/button/button";
+import { Input } from "../ui/input/input";
+import { Circle } from "../ui/circle/circle";
+import { nanoid } from "nanoid";
+import style from '../stack-page/stack-page.module.css'
+import { ElementStates } from "../../types/element-states";
+import sleep from "../../utils/sleep";
+
+const time = 300
 
 export const QueuePage: React.FC = () => {
+  const [value, setValue] = useState('')
+  const [loader, setLoader] = useState({name: 'default', loading: false})
+  const [lastIndex, setLastIndex] = useState(0)
+
+  const [stack, setStack] = useState<Array<string>>(['', '', '', '', '', '', ''])
+  const [queue, setQueue] = useState<Array<string>>(['', '', '', '', '', '', ''])
+
+  const head = 0
+  const tail = 0
+
+  const enqueue = (item: string) => queue.push(item);
+  const dequeue = () => queue.shift();
+
+  const addElement = async () => {
+    setLoader({ name: 'add', loading: true})
+    stack.push(value)
+    setValue('')
+    setStack([...stack])
+    await sleep(time)
+    setLastIndex(lastIndex + 1)
+    setLoader({ ...loader})
+  }
+
+  const deleteElement = async () => {
+    setLoader({ name: 'delete', loading: true})
+    setLastIndex(lastIndex - 1)
+    await sleep(time)
+    stack.pop()
+    setStack([...stack])
+    setLoader({ ...loader})
+  }
+
+  const clearStack = () => {
+    setStack([])
+    setLastIndex(0)
+  }
+
+  const onChange = (e: ChangeEvent<HTMLInputElement>) => {
+    setValue(e.target.value)
+  }
+
   return (
     <SolutionLayout title="Очередь">
-
+      <div className={style.form}>
+        <Input
+          maxLength={4}
+          isLimitText
+          onChange={onChange}
+          value={value}
+        />
+        <Button
+          text="Добавить"
+          type="button"
+          disabled={!value || loader.loading || stack.length > 19}
+          onClick={addElement}
+          isLoader={loader.loading && loader.name === 'add'}
+        />
+        <Button
+          text="Удалить"
+          type="button"
+          disabled={!stack.length || loader.loading}
+          onClick={deleteElement}
+          isLoader={loader.loading && loader.name === 'delete'}
+        />
+        <Button
+          text="Очистить"
+          type="button"
+          disabled={!stack.length || loader.loading}
+          extraClass={style.button}
+          onClick={clearStack}
+        />
+      </div>
+      <ul className={style.symbolList}>
+        {stack?.map((item, index) => {
+          return (
+            <Circle
+              key={nanoid()}
+              index={index}
+              letter={item}
+              state={
+                index === lastIndex
+                  ? ElementStates.Changing
+                  : ElementStates.Default
+              }
+              head={stack.length - 1 === index ? "head" : ""}
+              tail={stack.length - 2 === index ? "tail" : ""}
+            />
+          );
+        })}
+      </ul>
     </SolutionLayout>
   );
 };
--- src/components/sorting-page/sorting-page.tsx
+++ src/components/sorting-page/sorting-page.tsx
@@ -7,122 +7,115 @@ import { nanoid } from "nanoid";
 import { Column } from "../ui/column/column";
 import sleep from "../../utils/sleep";
 import { ElementStates, TSorting } from "../../types/element-states";
+import { Direction } from "../../types/direction";
+
+const time = 100 //ms
 
 export const SortingPage: React.FC = () => {
   const [value, setValue] = useState('choice')
-  const [loader, setLoader] = useState({name: 'decrease', loading: false})
-  const [arr, setArr] = useState<Array<TSorting>>()
+  const [loader, setLoader] = useState({name: 'default', loading: false})
+  const [arr, setArr] = useState<Array<TSorting>>([])
+  const { length } = arr;
 
   const randomArr = () => {
-    const lengthArr = Math.floor(Math.random() * (14 + 1) + 3)
+    const max = 17
+    const min = 3
+    const lengthArr = Math.floor(Math.random() * (max - min + 1) + min)
     const array: TSorting[] = []
     for(let i = 0; i < lengthArr; i++ ) {
-      let rnd = Math.floor(Math.random() * (100 + 1))
+      const maxNum = 100
+      let randomNum = Math.floor(Math.random() * (maxNum + 1))
       for(let j = 0; j < array.length; j++ ) {
-        if(rnd === array[j].index) {
+        if(randomNum === array[j].index) {
           array.splice(j, 1)
         }
       }
-      array.push({index: rnd, state: ElementStates.Default})
+      array.push({index: randomNum, state: ElementStates.Default})
     }
     setArr(array)
   }
 
   const reset = () => {
-    if (arr !== undefined) {
-      for (let i = 0; i < arr?.length; i++) {
-        arr[i].state = ElementStates.Default
-      }
-      setArr([...arr])
+    for (let i = 0; i < arr?.length; i++) {
+      arr[i].state = ElementStates.Default
     }
+    setArr([...arr])
   }
 
   const swap = (arr: TSorting[], i: number, l: number) => {
     return [arr[i], arr[l]] = [arr[l], arr[i]]
   }
 
-  const decrease = async () => {
-    setLoader({ name: 'decrease', loading: true })
-    reset()
-    if (arr !== undefined) {
-      const { length } = arr;
-      if (value === 'choice') {
-        for (let i = 0; i < length; i++) {
+  const choice = async (name: string) => {
+    for (let i = 0; i < length; i++) {
 
-          for (let j = i; j < length; j++) {
-            arr[i].state = ElementStates.Changing
-            arr[j].state = ElementStates.Changing
-            setArr([...arr])
-            await sleep(500)
-            if (arr[i].index < arr[j].index) { swap(arr, i, j) }
-            arr[i].state = ElementStates.Default
-            arr[j].state = ElementStates.Default
-          }
-
-          arr[i].state = ElementStates.Modified
-          setArr([...arr])
+      for (let j = i; j < length; j++) {
+        arr[i].state = ElementStates.Changing
+        arr[j].state = ElementStates.Changing
+        setArr([...arr])
+        await sleep(time)
+        if (name === 'increase') {
+          if (arr[i].index > arr[j].index) { swap(arr, i, j) }
         }
-      } else {
-        for (let i = 0; i < length; i++) {
-          for (let j = 0; j < length - i - 1; j++) {
-            arr[j].state = ElementStates.Changing
-            arr[j + 1].state = ElementStates.Changing
-            setArr([...arr])
-            await sleep(1000)
-            if (arr[j].index < arr[j + 1].index) { swap(arr, j, j + 1) }
-            arr[j].state = ElementStates.Default
-            arr[j + 1].state = ElementStates.Default
-          }
-          arr[length - i - 1].state = ElementStates.Modified
-          setArr([...arr])
+        if (name === 'decrease') {
+          if (arr[i].index < arr[j].index) { swap(arr, i, j) }
         }
+        arr[i].state = ElementStates.Default
+        arr[j].state = ElementStates.Default
       }
+
+      arr[i].state = ElementStates.Modified
+      setArr([...arr])
     }
-    setLoader({ ...loader, loading: false })
+  }
+
+  const bubble = async (name: string) => {
+    for (let i = 0; i < length; i++) {
+      for (let j = 0; j < length - i - 1; j++) {
+        arr[j].state = ElementStates.Changing
+        arr[j + 1].state = ElementStates.Changing
+        setArr([...arr])
+        await sleep(time)
+        if (name === 'increase') {
+          if (arr[j].index > arr[j + 1].index) { swap(arr, j, j + 1) }
+        }
+        if (name === 'decrease') {
+          if (arr[j].index < arr[j + 1].index) { swap(arr, j, j + 1) }
+        }
+        arr[j].state = ElementStates.Default
+        arr[j + 1].state = ElementStates.Default
+      }
+      arr[length - i - 1].state = ElementStates.Modified
+      setArr([...arr])
+    }
+  }
+
+  const decrease = async () => {
+    const type = 'decrease'
+    setLoader({ name: type, loading: true })
+    reset()
+    
+    if (value === 'choice') {
+      await choice(type)
+    } else {
+      await bubble(type)
+    }
+    
+    setLoader({ ...loader })
   }
 
   const increase = (async () => {
-    setLoader({name: 'increase', loading: true})
+    const type = 'increase'
+    setLoader({name: type, loading: true})
     reset()
-    if (arr !== undefined) {
-      const { length } = arr;
 
-      if (value === 'choice') {
-
-        for (let i = 0; i < length; i++) {
-
-          for (let j = i; j < length; j++) {
-            arr[i].state = ElementStates.Changing
-            arr[j].state = ElementStates.Changing
-            setArr([...arr])
-            await sleep(500)
-            if (arr[i].index > arr[j].index) { swap(arr, i, j) }
-            arr[i].state = ElementStates.Default
-            arr[j].state = ElementStates.Default
-          }
-
-          arr[i].state = ElementStates.Modified
-          setArr([...arr])
-        }
-
-      } else {
-        for (let i = 0; i < length; i++) {
-          for (let j = 0; j < length - i - 1; j++) {
-            arr[j].state = ElementStates.Changing
-            arr[j + 1].state = ElementStates.Changing
-            setArr([...arr])
-            await sleep(500)
-            if (arr[j].index > arr[j + 1].index) { swap(arr, j, j + 1) }
-            arr[j].state = ElementStates.Default
-            arr[j + 1].state = ElementStates.Default
-          }
-          arr[length - i - 1].state = ElementStates.Modified
-          setArr([...arr])
-        }
-      }
+    if (value === 'choice') {
+      await choice(type)
+    } else {
+      await bubble(type)
     }
-    
-    setLoader({...loader, loading: false})
+
+    setLoader({...loader})
   })
 
   const onChange = (e: ChangeEvent<HTMLInputElement>) => {
@@ -154,18 +147,22 @@ export const SortingPage: React.FC = () => {
           isLoader={loader.loading && loader.name === 'increase'}
           onClick={increase}
           disabled={!arr || (loader.name !== 'increase' && loader.loading)}
+          sorting={Direction.Ascending}
+          extraClass={style.button}
         />
         <Button
           text="По убыванию"
           isLoader={loader.loading && loader.name === 'decrease'}
           onClick={decrease}
           disabled={!arr || (loader.name !== 'decrease' && loader.loading)}
-          style={{marginRight: 68}}
+          sorting={Direction.Descending}
+          extraClass={style.button}
         />
         <Button
           text="Новый массив"
           onClick={randomArr}
           disabled={loader.loading}
+          extraClass={style.buttonArr}
         />
       </div>
       <div className={style.chart} >
